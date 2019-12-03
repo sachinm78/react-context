5cabc93a0409e6336a97144485090e040921a834
sachinm78 <sachinm78@gmail.com>
2019-12-03 15:15:18 -0600

    successfully used Context API to manage state for a toggled theme

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,14 @@
 import React from "react"
-import PropTypes from "prop-types"
 import {ThemeContextConsumer} from "./themeContext"
 
 function Button(props) {
     return (
         <ThemeContextConsumer>
-            {theme => (
-                <button className={`${theme}-theme`}>Switch Theme</button>
+            {context => (
+                <button onClick={context.toggleTheme} className={`${context.theme}-theme`}>Switch Theme</button>
             )}
         </ThemeContextConsumer>
     )    
-}
-
-Button.propTypes = {
-    theme: PropTypes.oneOf(["light", "dark"])
-}
-
-Button.defaultProps = {
-    theme: "light"
 }
 
 export default Button
