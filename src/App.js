67e80b960ec28f12d3451ffbe11cd2f0236df5d5
sachinm78 <sachinm78@gmail.com>
2019-12-03 15:07:00 -0600

    refactored themeContext.js to be a stateful component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from "react"
 
 import Header from "./Header"
 import Button from "./Button"
-import ThemeContext from "./themeContext"
 import styles from "./index.css"
 
 function App() {
     return (
         <div>
             <Header />
-            <ThemeContext.Consumer>
-                {theme => (
-                    <Button theme={theme} />
-                )}
-            </ThemeContext.Consumer>
-            
-            <Button theme="light" />
+            <Button />
         </div>
     )
 }
--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {ThemeContextConsumer} from "./themeContext"
 
 function Button(props) {
     return (
-        <button className={`${props.theme}-theme`}>Switch Theme</button>
+        <ThemeContextConsumer>
+            {theme => (
+                <button className={`${theme}-theme`}>Switch Theme</button>
+            )}
+        </ThemeContextConsumer>
     )    
 }
 
