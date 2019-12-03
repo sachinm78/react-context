2e6c2249094b60508c9d7b832e0cb52b7d75b690
sachinm78 <sachinm78@gmail.com>
2019-12-03 14:52:55 -0600

    fixed issue with css styles not loading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import Header from "./Header"
 import Button from "./Button"
 import ThemeContext from "./themeContext"
+import styles from "./index.css"
 
 function App() {
     return (
--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,18 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 function Button(props) {
     return (
         <button className={`${props.theme}-theme`}>Switch Theme</button>
     )    
+}
+
+Button.propTypes = {
+    theme: PropTypes.oneOf(["light", "dark"])
+}
+
+Button.defaultProps = {
+    theme: "light"
 }
 
 export default Button
