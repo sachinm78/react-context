01ebdd3f0ff5ec7c41d32175aba4644da7c0ade3
sachinm78 <sachinm78@gmail.com>
2019-12-30 13:39:11 -0600

    refactored using useContext hook

--- src/themeContext.js
+++ src/themeContext.js
@@ -1,12 +1,5 @@
 import React, {Component} from "react"
-const {Provider, Consumer} = React.createContext()
-
-/**
- * Challenge:
- * 1) Add state to hold the current theme
- * 2) Add a method for flipping the state between light and dark
- * 
- */
+const ThemeContext = React.createContext()
 
 class ThemeContextProvider extends Component {
     state = {
@@ -23,11 +16,11 @@ class ThemeContextProvider extends Component {
     
     render() {
         return (
-            <Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
+            <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
                 {this.props.children}
-            </Provider>
+            </ThemeContext.Provider>
         )
     }
 }
 
-export {ThemeContextProvider, Consumer as ThemeContextConsumer}
+export {ThemeContextProvider, ThemeContext}
